e667e55b4cb1fce28f5246c5011ec72acc423f2f
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2021-10-26 15:48:03 -0700

    Changing links on homepage to point to examples.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
@@ -23,33 +24,35 @@ const Home: NextPage = () => {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <Link href="/ssr">
+            <a className={styles.card}>
+              <h2>SSR &rarr;</h2>
+              <p>Example using Server Side Rendering</p>
+            </a>
+          </Link>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <Link href="/ssg">
+            <a className={styles.card}>
+              <h2>SSG &rarr;</h2>
+              <p>Example using Static Site Generation</p>
+            </a>
+          </Link>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+          <Link href="/csr">
+            <a className={styles.card}>
+              <h2>CSR &rarr;</h2>
+              <p>Example using Client Side Rendering</p>
+            </a>
+          </Link>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <Link href="/ssgWithIsr">
+            <a className={styles.card}>
+              <h2>SSG with ISR &rarr;</h2>
+              <p>
+                Example using Static Site Generation and Incremental Static Regeneration
+              </p>
+            </a>
+          </Link>
         </div>
       </main>
 
