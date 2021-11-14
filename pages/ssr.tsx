b4c9b408376ce3f74e46df47c93f668d6f94b9e2
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2021-11-14 11:02:16 -0800

    Transitioning ssg and ssr to use ISS api calls

--- pages/ssr.tsx
+++ pages/ssr.tsx
@@ -1,22 +1,23 @@
 import { GetServerSideProps } from 'next'
+import { IssData } from '../interfaces/iss';
 import styles from '../styles/Home.module.css'
 
 export const getServerSideProps: GetServerSideProps = async () => {
-    
-    const date = new Date().toLocaleDateString();
-    const time = new Date().toLocaleTimeString();
-
-    return {
-        props: { date, time }
-    };
+    const response = await fetch('http://api.open-notify.org/iss-now.json')
+    const data: IssData = await response.json()
+  
+    return { props: { data } }
 }
 
-function Ssr({ date, time }: { date: string, time: string }) {
+function Ssr({ data}: { data: IssData }) {
     return (
-        <div className={styles.clock}>
-            <div>SSR</div>
-            {date} {time}
-        </div>
+        <main>
+            <h1>Server Side Rendering (SSR)</h1>
+            <h3>{data?.iss_position?.latitude}</h3>
+            <h3>{data?.iss_position?.longitude}</h3>
+            <h3>{new Date(data.timestamp *1000).toLocaleDateString()}</h3>
+            <h3>{new Date(data.timestamp *1000).toLocaleTimeString()}</h3>
+        </main>
     )
 }
 
